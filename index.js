0ff43abadc1b47b9b4135de3587ad0ee1f0685d5
Mona Lisa <patrinoua@gmail.com>
2018-07-01 12:36:12 +0200

    changed the port to have also process.env.PORT

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const hb = require('express-handlebars');
 const fs = require('fs');
 
 let myProjects = fs.readdirSync(__dirname+'/projects');
+
 var myProjectObjects = myProjects.map(function(project){
     return {
         dirName: project,
@@ -20,78 +21,20 @@ var myProjectObjects = myProjects.map(function(project){
 })
 
 app.engine('handlebars',hb());
+
 app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname+'/public'));
+
 app.use(express.static(__dirname+'/projects'));
 
 app.use(bodyParser.urlencoded({
     extended: false
 }));
-
 app.get('/',function(req,res){
     res.render('projectData',{
         myProjectObjects : myProjectObjects,
         layout:'welcome'
     })
 })
-app.listen(8080, ()=> console.log(`port 8080 is listening`) );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// app.get('/projects/:projectName',function(req,res,next){
-// var requestedProject=0;
-//     myProjects.forEach(project =>{
-//         if(req.params.projectName == project){
-//             console.log('we have a match!!!!!!!!');
-//             requestedProject=project;
-//         }
-//         else {
-//             console.log('no such project try again!');
-//         }
-//     })
-//     console.log(requestedProject);
-//     if(requestedProject!=0){
-//         var project = {
-//                 dirName: requestedProject,
-//                 displayName: require('./projects/' + requestedProject + '/project.json' ).displayName,
-//                 description:  require('./projects/' + requestedProject + '/project.json' ).description
-//             }
-//         res.render('project',{
-//             myProjectObjects : myProjectObjects,
-//             project : project,
-//             layout:'each_project_layout'
-//         })
-//     }else {
-//         res.status(404).send(`<!doctype html><title></title><h1>Page  "${req.params.projectName}" not found boo :( !</h1>`);
-//     }
-// });
+app.listen(process.env.PORT || 8080, ()=> console.log(`port 8080 is listening`) );
