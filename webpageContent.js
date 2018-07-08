0c00eb5e9f51048ca15ea4af8ce58dab6c946fe7
Mona Lisa <patrinoua@gmail.com>
2018-07-09 01:22:00 +0200

    english and greek version

---
+++ webpageContent.js
@@ -0,0 +1,38 @@
+
+exports.contentEn={
+    name:'Angeliki Patrinou',
+
+    about:'About',
+    work:'Work',
+    contact:'Contact',
+
+    title:'Full Stack Developer',
+
+    personalDescription:`
+        I am a Full Stack Developer, based in Berlin.
+        I have studied Applied Mathematics and Management,
+        and my professional experience includes online marketing
+        and dynamical systems consulting.
+        I like creating websites that work well and feel good,
+        by using the latest technologies.`,
+
+    myFont:'Quicksand'
+}
+exports.contentGr={
+    name:'Αγγελική Πατρινού',
+
+    about:'Σχετικά',
+    work:'Πορτφόλιο',
+    contact:'Επικοινωνία',
+
+    title:'Σχεδιασμός Ιστοσελίδων',
+
+    personalDescription:`
+        Με λένε Αγγελική και ζω στο Βερολίνο.
+        Έχω σπουδάσει Μαθηματικά και Διοίκηση Επιχειρήσεων
+        και έχω εργαστεί στο Online Marketing, και την προώθηση και ως Σύμβουλος Επιχειρήσεων.
+        Μου αρέσει να φτιάχνω ιστοσελίδες που λειτουργούνε καλά
+        και φαίνονται ωραία, χρησιμοποιώντας της τελευταίες τεχνολογίες.
+        `,
+    myFont:'Helvetica'
+}
