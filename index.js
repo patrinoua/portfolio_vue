359de57ca7c4c5e0c8b7b151bfa5abef8f7caa34
Mona Lisa <patrinoua@gmail.com>
2018-12-22 16:52:40 +0200

    add starwars project

--- index.js
+++ index.js
@@ -1,67 +1,76 @@
-const express = require('express');
-const app=express();
-const bodyParser = require('body-parser');
-const hb = require('express-handlebars');
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const hb = require('express-handlebars')
+const fs = require('fs')
 
-const {contentEn, contentGr} =require('./webpageContent.js');
+const { contentEn, contentGr } = require('./webpageContent.js')
 
-
-app.engine('handlebars',hb());
-app.set('view engine', 'handlebars');
-app.use(express.static(__dirname+'/public'));
-app.use(express.static(__dirname+'/projects'));
-app.use(bodyParser.urlencoded({
+app.engine('handlebars', hb())
+app.set('view engine', 'handlebars')
+app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/projects'))
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+)
 
-let myProjects = fs.readdirSync(__dirname+'/projects');
+let myProjects = fs.readdirSync(__dirname + '/projects')
 
-var myProjectObjects = myProjects.map(function(project){
-    return {
-        dirName: project,
-        heroku_url: require('./projects/' + project + '/project.json' ).heroku_url,
-        github_url: require('./projects/' + project + '/project.json' ).github_url,
-        createdWith: require('./projects/' + project + '/project.json' ).createdWith,
-        comment_link: require('./projects/' + project + '/project.json' ).comment_link,
-        comment_url: require('./projects/' + project + '/project.json' ).comment_url,
-        displayName: require('./projects/' + project + '/project.json' ).displayName,
-        description: require('./projects/' + project + '/project.json' ).description,
-        tryMsg: require('./projects/' + project + '/project.json' ).tryMsg,
-        codeMsg: require('./projects/' + project + '/project.json' ).codeMsg,
-        comment: require('./projects/' + project + '/project.json' ).comment
-    }
-});
-var myProjectObjectsGr = myProjects.map(function(project){
-    return {
-        dirName: project,
-        heroku_url: require('./projects/' + project + '/project.json' ).heroku_url,
-        github_url: require('./projects/' + project + '/project.json' ).github_url,
-        comment_link: require('./projects/' + project + '/project.json' ).comment_link,
-        comment_url: require('./projects/' + project + '/project.json' ).comment_url,
-        displayName: require('./projects/' + project + '/project.json' ).displayNameGr ||"displaynamestaellliiinikkaaaa",
-        description: require('./projects/' + project + '/project.json' ).descriptionGr,
-        tryMsg: require('./projects/' + project + '/project.json' ).tryMsgGr,
-        codeMsg: require('./projects/' + project + '/project.json' ).codeMsgGr,
-        createdWith: require('./projects/' + project + '/project.json' ).createdWithGr,
-        comment:require('./projects/' + project + '/project.json' ).commentGr
-    }
-});
-
-app.get('/',function(req,res){
-    res.render('projectData',{
-        content:contentEn,
-        myProjectObjects:myProjectObjects,
-        layout:'welcome'
-    })
+var myProjectObjects = myProjects.map(function(project) {
+  return {
+    dirName: project,
+    heroku_url: require('./projects/' + project + '/project.json').heroku_url,
+    github_url: require('./projects/' + project + '/project.json').github_url,
+    createdWith: require('./projects/' + project + '/project.json').createdWith,
+    comment_link: require('./projects/' + project + '/project.json')
+      .comment_link,
+    comment_url: require('./projects/' + project + '/project.json').comment_url,
+    displayName: require('./projects/' + project + '/project.json').displayName,
+    description: require('./projects/' + project + '/project.json').description,
+    tryMsg: require('./projects/' + project + '/project.json').tryMsg,
+    codeMsg: require('./projects/' + project + '/project.json').codeMsg,
+    comment: require('./projects/' + project + '/project.json').comment
+  }
+})
+var myProjectObjectsGr = myProjects.map(function(project) {
+  return {
+    dirName: project,
+    heroku_url: require('./projects/' + project + '/project.json').heroku_url,
+    github_url: require('./projects/' + project + '/project.json').github_url,
+    comment_link: require('./projects/' + project + '/project.json')
+      .comment_link,
+    comment_url: require('./projects/' + project + '/project.json').comment_url,
+    displayName:
+      require('./projects/' + project + '/project.json').displayNameGr ||
+      'displaynamestaellliiinikkaaaa',
+    description: require('./projects/' + project + '/project.json')
+      .descriptionGr,
+    tryMsg: require('./projects/' + project + '/project.json').tryMsgGr,
+    codeMsg: require('./projects/' + project + '/project.json').codeMsgGr,
+    createdWith: require('./projects/' + project + '/project.json')
+      .createdWithGr,
+    comment: require('./projects/' + project + '/project.json').commentGr
+  }
 })
 
-app.get('/gr',function(req,res){
-    res.render('projectData',{
-        content:contentGr,
-        myProjectObjects:myProjectObjectsGr,
-        // projectsText:projectsTextGr,
-        layout:'welcome'
-    })
+app.get('/', function(req, res) {
+  res.render('projectData', {
+    content: contentEn,
+    myProjectObjects: myProjectObjects,
+    layout: 'welcome'
+  })
 })
-app.listen(process.env.PORT || 8080, ()=> console.log(`port 8080 is listening`) );
+
+app.get('/gr', function(req, res) {
+  res.render('projectData', {
+    content: contentGr,
+    myProjectObjects: myProjectObjectsGr,
+    // projectsText:projectsTextGr,
+    layout: 'welcome'
+  })
+})
+app.listen(process.env.PORT || 8080, () =>
+  console.log(`port 8080 is listening`)
+)
--- webpageContent.js
+++ webpageContent.js
@@ -1,15 +1,14 @@
+exports.contentEn = {
+  name: 'Angeliki Patrinou',
 
-exports.contentEn={
-    name:'Angeliki Patrinou',
+  about: 'About',
+  work: 'Work',
+  contact: 'Contact',
 
-    about:'About',
-    work:'Work',
-    contact:'Contact',
+  title: 'Full Stack Developer',
 
-    title:'Full Stack Developer',
-
-    personalDescription:`
-        I am a professional Web Developer that lives and works in Berlin.
+  personalDescription: `
+        I am a Fullstack Web Developer that lives and works in Berlin.
         I have studied Applied Mathematics and Management,
         and my professional experience includes Online Marketing
         and Dynamical Systems Consulting.
@@ -17,19 +16,19 @@ exports.contentEn={
         I like creating Websites that work well and feel good,
         by using the latest technologies.`,
 
-    myFont:'Quicksand'
+  myFont: 'Quicksand'
 }
-exports.contentGr={
-    name:'Αγγελική Πατρινού',
+exports.contentGr = {
+  name: 'Αγγελική Πατρινού',
 
-    about:'Σχετικά',
-    work:'Πορτφόλιο',
-    contact:'Επικοινωνία',
+  about: 'Σχετικά',
+  work: 'Πορτφόλιο',
+  contact: 'Επικοινωνία',
 
-    title:'Σχεδιασμός Ιστοσελίδων',
+  title: 'Σχεδιασμός Ιστοσελίδων',
 
-    personalDescription:`
-        Με λένε Αγγελική, είμαι επαγγελματίας δημιουργός ιστοσελίδων
+  personalDescription: `
+        Με λένε Αγγελική, είμαι δημιουργός ιστοσελίδων
         και τα τελευταία χρόνια ζω και εργάζομαι στο Βερολίνο.
         Έχω σπουδάσει Μαθηματικά και Διοίκηση Επιχειρήσεων
         και στο παρελθόν έχω εργαστεί ως σύμβουλος Online Marketing και
@@ -37,5 +36,5 @@ exports.contentGr={
         Μου αρέσει να φτιάχνω εύχρηστες ιστοσελίδες με ωραίο design,
         χρησιμοποιώντας της τελευταίες τεχνολογίες.
         `,
-    myFont:'Helvetica'
+  myFont: 'Helvetica'
 }
