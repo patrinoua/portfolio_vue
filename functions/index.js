ff59fd1591cd784e52d43ef99307c35d31a9cfec
Mona Lisa <patrinoua@gmail.com>
2019-07-08 00:43:27 +0300

    WIP-firebase setup

---
+++ functions/index.js
@@ -0,0 +1,15 @@
+const functions = require('firebase-functions')
+const express = require('express')
+
+const app = express()
+
+app.get('/timestamp', (request, response) => {
+  console.log('got it!')
+  response.send(`${Date.now()}`)
+})
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.app = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!')
+})
